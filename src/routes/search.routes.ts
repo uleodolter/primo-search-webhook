8ae35454ad869e67c711db4a9fb817cfc63885aa
Ulrich Leodolter <ulrich.leodolter@obvsg.at>
2023-11-26 19:53:47 +0100

    morgan combined logging cleanup search routes

--- src/routes/search.routes.ts
+++ src/routes/search.routes.ts
@@ -10,20 +10,8 @@ class SearchRoutes {
   }
 
   intializeRoutes() {
-    // Create a new Search
-    this.router.post("/", this.controller.create);
-
     // Retrieve all Searchs
     this.router.get("/", this.controller.findAll);
-
-    // Retrieve a single Search with id
-    this.router.get("/:id", this.controller.findOne);
-
-    // Update a Search with id
-    this.router.put("/:id", this.controller.update);
-
-    // Delete a Search with id
-    this.router.delete("/:id", this.controller.delete);
   }
 }
 
